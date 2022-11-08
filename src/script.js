ed3fa3602b8920df6db830567c48ce66bdbd5e25
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-11-08 11:00:55 +0530

    JavaScript - ToDo App

--- src/script.js
+++ src/script.js
@@ -1,34 +1,71 @@
-let data_array = [];
-
+let data_array = []; //Declaring data array for todo
+//------------------- Add Function-------------
 function add() {
-    // alert('ok');
-
     var item = document.getElementById('new-task').value;
-    // alert(item);
-    //object creation
-    var object = { item: item, status: 'incomplete' };
-    console.log(object);
-    // pushing object values in an array
-    data_array.push(object);
-
-    
-        //    showing result in table tag
-        var str = "<ul>"
-        data_array.forEach(element => {
-            str += "<li><input type=checkbox><label>"+element.item+"</label><input type=text><button class=edit onclick='edit()'>Edit</button><button class=delete>Delete</button></li>"
-
+    if (item == "") { //checking input field is blank or not---
+        var msg = "Can't Add Blank Field.!!";
+        document.getElementById('msg').innerHTML = msg;
+    } else {
+        var object = { item: item, status: 'incomplete' }; //array object creation---
+        data_array.push(object); //pushing data into array------
+        document.getElementById('new-task').value = "";
+        document.getElementById('msg').innerText = "";
+        display();
+    }
+}
+// Display function for showing ToDo----
+function display() {
+    var str = "<ul id='incomplete-tasks'>"
+    for (var index = 0; index < data_array.length; index++) {
+        if (data_array[index].status == "incomplete") { //For incompleted status task----
+            str += "<li><input type='checkbox' id=" + index + " onclick='checkbox(this)'><label>" + data_array[index].item + "</label><button  id=" + index + " onclick='edit(this)'>Edit</button><button id=" + index + " onclick='del(this)'>Delete</button></li>"
             str += "</ul>"
-
-            // console.log(str);
-            document.getElementById('incomplete-tasks').innerHTML = str;
-        })
-        var item = document.getElementById('new-task').value=""; 
-
+        }
+    }
+    var strr = " <h3>Completed</h3>"
+    strr += "<ul id='completed-tasks'>"
+    for (var index = 0; index < data_array.length; index++) {
+        if (data_array[index].status == "complete") { //For complted status task----
+            strr += "<li><input type='checkbox'  id=" + index + " onclick='checkbox(this)' checked><label>" + data_array[index].item + "</label><button id=" + index + "  onclick='edit(this)'>Edit</button><button id=" + index + " onclick='del(this)'>Delete</button></li>"
+            strr += "</ul>"
+        }
+    }
+    document.getElementById('todo').innerHTML = str + strr;
 }
-function edit() {
-    //alert('edit');
-
-    
-
+// -----------Checkbox Function for updating Task Status-------
+function checkbox(res) {
+    var id = res.id;
+    if (data_array[id].status == "incomplete") { //changing status 'incomplete' to 'complete'
+        data_array[id].status = "complete";
+        display();
+    }
+    else { //changing status 'complete' to 'incomplete'
+        data_array[id].status = "incomplete";
+        display();
+    }
 }
-
+// ---------Delete Function For Deleting Task---------------
+function del(res) {
+    var index = res.id;
+    data_array.splice(index, 1); //Deleteing data with index id--
+    display();
+}
+// -----Edit Function for edit an task-----------------
+function edit(res) {
+    var index = res.id;
+    var task_name = data_array[index].item;
+    document.getElementById('new-task').value = task_name;
+    document.getElementById("add").style.display = 'none'; //Hiding Add Button
+    document.getElementById("update").style.display = 'block'; // Displaying Update Button
+    document.getElementById("update").setAttribute('value', index); //Seting Value Attribute on Update Button
+}
+// ----------Update Function for Set new value on clicked index-----------
+function update(res) {
+    var index = res.value;
+    var item = document.getElementById('new-task').value;
+    data_array[index].item = item;
+    document.getElementById("add").style.display = 'block'; //Displaying Add Button
+    document.getElementById("update").style.display = 'none'; //Hiding Update Button
+    document.getElementById('new-task').value = "";
+    display();
+}
